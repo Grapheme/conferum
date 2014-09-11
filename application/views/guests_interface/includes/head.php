1639af48bcd8401a1a6373a1404add6407a901b4
Владимир <vkharseev@gmail.com>
2014-09-11 13:33:51 +0400

    Цена

--- application/views/guests_interface/includes/head.php
+++ application/views/guests_interface/includes/head.php
@@ -7,7 +7,7 @@ if(!isset($page_content['page_keywords'])):
     $page_content['page_keywords'] = '';
 endif;
 ?>
-<meta name="keywords" content="<?=$page_content['page_keywords']);?>">
+<meta name="keywords" content="<?=$page_content['page_keywords'];?>">
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <meta name = "format-detection" content = "telephone=no" />
 <link rel="stylesheet" href="<?=base_url('css/bootstrap.css');?>" />
